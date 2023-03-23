c5323489ef54ba49c2d0bca7981033885e7d5087
washington_silva <wssaleph@gmail.com>
2023-03-22 21:34:46 -0300

    Aula 2 finalizada

---
+++ scripts/countdown.R
@@ -0,0 +1,7 @@
+`r countdown(1, 30, style = "position: relative; width: min-content;")`
+
+`r countdown(minutes = 5, seconds = 00)`
+
+`r countdown(minutes = 5, seconds = 00)`
+
+`r countdown(minutes = 5, seconds = 00)`
